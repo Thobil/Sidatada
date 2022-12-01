7d99b266518f477e3f0bd1c3d71c7885f992cee1
Benoit Gaudet <bgaudet38@gmail.com>
2022-12-01 22:44:39 +0100

    Get the score from the local storage

--- scripts/end.js
+++ scripts/end.js
@@ -1,20 +1,27 @@
 window.onload = (event) => {
     //code here
     console.log("page is fully loaded");
-    displayTextAfterTime();
-};
 
-async function displayTextAfterTime() {
-    var array = ["Vous avez 0% de bonnes réponses",
+    // get the score in the local storage
+    let score = localStorage.getItem("score");
+    // Default score value if score isn't in the local storage
+    if (score == null)
+        score = 0
+    // Set the sentences to display
+    var array = ["Vous avez " + score + "% de bonnes réponses",
                 "On ne peut pas reconnaître une personne atteinte du SIDA",
                 "Protégez-vous !"];
+    // display them smoothly
+    displayTextAfterTime(array, score);
+};
+
+async function displayTextAfterTime(array, score) {
     var id_prefix = "msg-";
 
     for (let index = 0; index < array.length; index++) {
         document.getElementById("messages-container").innerHTML += "<span id=\"" + id_prefix + index + "\">" + array[index] + "</span>";
         document.getElementById("messages-container").style.visibility = "hidden";
     }
-
 
     for (let index = 0; index < array.length; index++) {
         await sleep(2000);
